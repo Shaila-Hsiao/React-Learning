5c7075635e9e0cb2cdef4d6a0dc7ed6f156a8ac6
yst200089 <87116920+yst200089@users.noreply.github.com>
2022-08-06 16:33:02 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -7,22 +7,27 @@ import SecondPage from "./pages/SecondPage";
 // import Login from './pages/Authorities/Login';
 // import Signup from './pages/Authorities/SignUp';
 import Login from './components/Login/Login';
+import album from './components/album/album';
+// import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+// import PhotoCamera from '@mui/icons-material';
 
 function App() {
   const [token, setToken] = useState();
+
   if(!token) {
     return <Login setToken={setToken} />
   }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/SecondPage" element={<SecondPage />} />
-          
-          
         </Route>
       </Routes>
+      <Route path='/album' element={<album />}>
+      </Route>
     </BrowserRouter>
 
   );
